fea1fb8b9922ba3eb7814ef8411836221e263098
mmann1123 <mmann1123@gmail.com>
2025-10-22 13:26:41 -0400

    update install on lanch

--- launch_gui.bat
+++ launch_gui.bat
@@ -3,6 +3,13 @@ echo ================================================================
 echo     GWU Course Calendar Scraper - GUI Launcher
 echo ================================================================
 echo.
+echo Checking and installing required packages...
+echo.
+
+python -m pip install --quiet --upgrade pip
+python -m pip install --quiet -r requirements.txt
+
+echo.
 echo Launching graphical interface...
 echo.
 
